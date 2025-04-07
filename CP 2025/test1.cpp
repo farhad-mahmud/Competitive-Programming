88ad25c6185c6fb55845dc7293dd5949055f00e2
farhad-mahmud <rixonahmed@gmail.com>
2025-04-08 04:45:00 +0600

    solved 1352B

--- CP 2025/test1.cpp
+++ CP 2025/test1.cpp
@@ -2,19 +2,35 @@
 
 using namespace std;
 
-#define ll long long
+#define int long long
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 #define yes cout << "YES\n";
 #define no cout << "NO\n";
 
-const ll MOD = 1e9 + 7;
+const int  MOD = 1e9 + 7;
 void solve ()
 {
         
      
-                    cout << "hello" << endl; 
+                   if(n % 2 == 0  && k > n/2 && k < n){
+
+
+                               no ;
+
+                              return ;
+                     }
+                     else if (n % 2 ==1 && k % 2 ==0){
+
+                                no ;
+                                return ;
+                     }
+                     else if (n < k){
+
+                              no ;
+                              return ;
+                     }   
 }
 
 
