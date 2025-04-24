4bb2bc125afbb68c003e38c652cd6050ef7ccce1
farhad-mahmud <rixonahmed@gmail.com>
2025-04-25 04:07:06 +0600

    solved 1020C

--- CP 2025/1020C.cpp
+++ CP 2025/1020C.cpp
@@ -13,26 +13,78 @@ const int  MOD = 1e9 + 7;
 void solve ()
 {
         
-                        int n , k  ;  cin >> n >> k;
+         int n, m, i, j, k, sum = 0, ans = 0, f = 0;
 
-                        vector <int > a(n) ;
+        cin >> n >> k;
 
-                        vector <int > b(n) ;
+        int a[n + 5], b[n + 5],mii = INT_MAX, mxx = INT_MIN;
 
-                        for(int i=0 ;i<n;i++){
+        for (i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+        set<int>sums;
 
-                               cin >> a[i] ;
-                        }
+        for (i = 1; i <= n; i++)
+        {
+            cin >> b[i];
+            if (b[i] != -1)
+            {
+                sum = a[i] + b[i];
+                sums.insert(sum);
+            }
+        }
 
-                        for(int i=0;i<n;i++){
+        if (sums.size()==0)
+        {
+            sort(a + 1, a + n + 1);
+            int mi = a[n] - a[1];
+            ans = max(k - mi + 1, 0ll);
+            cout << ans << endl;
+
+            return ;
+           
+        }
+
+        if(sums.size()>1)
+        {
+            cout << 0 << endl;
+            
+            return ;
+        }
 
 
-                                  cin >> b[i] ;
-                        }
 
+        for (i = 1; i <= n; i++)
+        {
+            if (b[i] == -1)
+            {
+                int need = sum - a[i];
+                if (need >= 0 and need <= k){
+                    
+                      continue ;
+                }
+                else{
+                    f = 1;
+                }
+            }
+        }
+
+        if(!f) 
+         {
+            cout<<1<<endl;
+         }
+
+         else {
+
+            cout<<0<<endl;
+
+         }
+
+
+    }
                         
 
-}
 
 
 
