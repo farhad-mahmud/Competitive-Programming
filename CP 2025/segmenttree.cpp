d0ccc28afee27b579d2f8a056bcf23a6b256689f
farhad-mahmud <rixonahmed@gmail.com>
2025-04-25 17:30:02 +0600

    learning segment tree

---
+++ CP 2025/segmenttree.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+const int N = 1e5 + 9 ;
+
+int a[N] ;
+
+int t[N * 4] ;
+
+
+void build(int node , int begin, int end)
+{
+
+	  if(begin == end){
+
+	  	       return ;
+	  }
+
+       int l = 2*node , r= 2*node + 1 ;    // left node and right node ;
+
+       int mid = (begin + end) / 2 ;
+
+       build(l,begin,mid);
+
+       build(r,mid+1 , end) ;
+
+
+
+
+}
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+     
+              int n ; cin >> n; 
+
+              for(int i = 1; i<=n ;i++){
+
+
+              	         cin >> a[i] ;
+              }
+         
+          build(1,1,n) ;
+
+             
+              
+
+return 0 ;
+}
