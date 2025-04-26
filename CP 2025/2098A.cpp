b64af47680f4f4a3283e9d9be21921dd93d387f9
farhad-mahmud <rixonahmed@gmail.com>
2025-04-26 16:42:15 +0600

    solved 2098A

---
+++ CP 2025/2098A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll  long long
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+const int  MOD = 1e9 + 7;
+void solve() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < 10; i++) {
+        int need = 9 - i;
+
+
+        for (int j = i + 1; j < 10; j++) {
+            if (s[j] < s[i] && (s[j] - '0') >= need) {
+                swap(s[i], s[j]);
+            }
+        }
+    }
+
+    cout << s << "\n";
+}
+
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+         cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
