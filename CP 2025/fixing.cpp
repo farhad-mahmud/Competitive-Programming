d6afbef861d1291ee75dea431084fd1b7bd716c8
farhad-mahmud <rixonahmed@gmail.com>
2025-05-01 17:54:54 +0600

    fixing

--- CP 2025/fixing.cpp
+++ CP 2025/fixing.cpp
@@ -12,36 +12,54 @@ int32_t main(){
 
             while(t--){
 
+      int n;
+    cin >> n;
 
+    vector<vector<int>> g(n + 1, vector<int>(n + 1)); 
+ 
+    vector<int> p(2 * n + 1); 
+ 
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            cin >> g[i][j];
+         }
+    }
+ 
+ 
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            p[i + j] = g[i][j]; 
+         }
+    }
+ 
+  
+ 
+    set<int> used;
+ 
+    for (int i = 2; i <= 2 * n; ++i){
+ 
+             used.insert(p[i]);
+    }
+ 
+ 
+    for (int i = 1; i <= 2 * n; i++) {
+ 
+        if (!used.count(i)) {  
+ 
+            p[1] = i;
+            break;
+ 
+        }
+    }
+ 
+    for (int i = 1; i <= 2 * n; i++){
+          cout << p[i] << " ";
+    } 
+ 
+ 
+         cout << endl; 
+                       
 
-            	  int n , m , l , r ; cin >> n >> m >> l >> r ;
- 
-                   int dif = (n - m) ;
- 
-                   int new_r = 0 ;
-                 
-                     
-                      new_r = (r - dif) ;
- 
-                      int new_l = 0 ;
- 
-                     
- 
-                      if(new_r < 0) {
- 
-                        int l_increment = abs(0-new_r) ;
- 
-                           new_l = (l + l_increment ) ;
- 
-                           cout << new_l <<  " " << 0 << endl; 
- 
-                      }
-                      else {
- 
- 
-                             cout << l << " " << new_r << endl; 
-                      }
- 
 
 	       
             }
