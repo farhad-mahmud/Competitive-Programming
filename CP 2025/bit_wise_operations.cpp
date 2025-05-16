d280bdb00e21ca6324203caead3b29611a565443
farhad-mahmud <rixonahmed@gmail.com>
2025-05-16 17:10:06 +0600

    learning bitwise operations

---
+++ CP 2025/bit_wise_operations.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int  long long
+
+#define gcd(x,y)      __gcd(x,y)
+#define lcm(x,y)      ((x/gcd(x,y)) * y)
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+#define vi       vector<int > 
+#define  forr    for(auto &i : x) 
+const int  MOD = 1e9 + 7;
+
+void solve ()
+{
+              //int a = 1 , b = 5 ;
+
+             // int x = a^b ;
+
+              //cout << x << endl;
+
+               int y =3 ;
+
+              cout << (y << 3) << endl; 
+
+              //one left shift = 3*2 = 6 
+              // 2 left shift = 6*2  = 12
+              
+
+
+              
+
+}
+
+
+ // remember this 
+
+// optimise loop uses via implementing number theory / algorithm
+// optimise array elements calculation via number theory / algorithm
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+         //cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
