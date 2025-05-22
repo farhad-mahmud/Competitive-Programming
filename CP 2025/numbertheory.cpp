b0f1803a906f3e3f622f595ffccd65a8d10429dd
farhad-mahmud <rixonahmed@gmail.com>
2025-05-23 02:53:46 +0600

    practicing number theory

---
+++ CP 2025/numbertheory.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int  long long
+
+#define gcd(x,y)      __gcd(x,y)
+#define lcm(x,y)      ((x/gcd(x,y)) * y)
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+#define vi       vector<int > 
+#define  forr    for(auto &i : x) 
+const int  MOD = 1e9 + 7;
+
+
+void solve ()
+{
+             int n ; cin >> n;
+
+             int digit_sum = 0;
+
+             for(int i=1 ;i<=n ;i++){
+
+                  int x ; cin >> x ;
+
+                  while(x){
+
+                         digit_sum += x % 10 ;   // extracting digit and summing them 
+
+                         x /= 10 ;
+                  }
+             }
+
+             if(digit_sum % 3 == 0){
+
+                    cout << "Yes" << endl;
+             }
+             else {
+
+                      cout << "No" << endl; 
+             }
+            
+}
+
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+         cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
