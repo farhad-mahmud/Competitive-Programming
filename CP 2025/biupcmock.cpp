54a655dc2649efc3044d760c70089adbf76c4d45
farhad-mahmud <rixonahmed@gmail.com>
2025-05-24 02:22:45 +0600

    biupc preli mock

---
+++ CP 2025/biupcmock.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int  long long
+
+#define gcd(x,y)      __gcd(x,y)
+#define lcm(x,y)      ((x/gcd(x,y)) * y)
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+#define vi       vector<int > 
+#define  forr    for(auto &i : x) 
+const int  MOD = 1e9 + 7;
+
+
+void solve ()
+{
+                  int n ; cin >> n; 
+
+                  vector<int > a(n) ;
+
+                  for(int i=0;i<n;i++){
+
+                         cin >> a[i] ;
+                  }
+
+
+                  int pol = 0;
+
+                  int cri = 0 ;
+
+                  int cnt = 0  ;
+ 
+        //we have to count polices till a -1 ;
+
+
+
+                  for(int i=0;i<n;i++){
+                      
+                         if(a[i] >= 1){
+                              
+                              pol += a[i] ;
+
+                             // cri= 0 ;
+                         }
+                         
+                          if(a[i] == -1){
+
+                            if(pol >= 1){
+
+                                  pol-- ;
+                            }
+                            else if(pol == 0){
+
+                                  cri++ ;
+                            }
+                         }
+
+                  }
+
+
+
+                    cout << cri << endl; 
+
+         
+}
+
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+        // cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
---
+++ CP 2025/biupcmock1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int  long long
+
+#define gcd(x,y)      __gcd(x,y)
+#define lcm(x,y)      ((x/gcd(x,y)) * y)
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+#define vi       vector<int > 
+#define  forr    for(auto &i : x) 
+const int  MOD = 1e9 + 7;
+
+
+void solve ()
+{ 
+                   string s ; cin >> s;
+
+                   int len = s.length() ;
+
+                   string p = s ;
+
+                   reverse(s.begin(),s.end()) ;
+
+                   cout << p+s << endl ;
+
+
+
+}
+
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+        // cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
