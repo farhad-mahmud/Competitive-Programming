fde729f53d2856d087fcefeeece05e0b1a417c25
farhad-mahmud <rixonahmed@gmail.com>
2025-06-17 21:35:53 +0600

    cf A

---
+++ CP 2025/2121A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int  long long
+
+#define nl       "\n" 
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+// Constants
+const int N = 1e5 + 9;
+const int MOD = 1e9 + 7;
+
+
+void solve ()
+{
+                   int n , s ;cin >> n >> s;
+
+                   vector<int > a(n) ;
+
+                   for(int i=0;i<n;i++){
+
+                         cin >> a[i] ;
+                   }
+                  
+
+                    int mn = *min_element(a.begin() , a.end()) ;
+
+                    int mx = *max_element(a.begin() , a.end()) ;
+
+                   int dif1 = abs(s- mn) ;
+                 
+
+                   int dif2 = abs(s- mx) ;
+
+
+                   int ans = (mx-mn) + min(dif1,dif2) ;                 
+
+                   cout << ans << nl;
+
+
+       
+}
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+         cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
