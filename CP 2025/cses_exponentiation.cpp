ad0df607ec04064813c5fa386d4bca013b3b18fc
farhad-mahmud <rixonahmed@gmail.com>
2025-06-27 03:51:26 +0600

    cses exponentiation

--- CP 2025/cses_exponentiation.cpp
+++ CP 2025/cses_exponentiation.cpp
@@ -14,59 +14,36 @@ using namespace std;
 const int N = 1e5 + 9;
 const int MOD = 1e9 + 7;
 
+// tc is log(n) ;
 
-// circular exponentiation ;
+// divide and conqueror method with recursion ;;;
 
+int power(int x,int n, int mod){
+         
 
-int power(int x,int n){
+       if(n == 0 ) {
 
-      int cur = 1 % MOD ; // initially ans = 1 ;
+            return 1 % mod ;
+       }
 
-      vector<int > v ;
+       if(x == 0 && n == 0 ){
 
-      v.push_back(1) ;
+            return 1 % mod ;
+       }
+      if(n % 2 == 0){  // n even ;
 
-      vector<bool > vis(MOD,false ) ;
+           int cur = power(x , n/2 ,mod ) ;
 
-      vis[1] = true ;
-      int start = 0 ;
+           return 1LL * cur * cur % mod ;  // cur*cur = x^(n/2) * x^(n/2)  == x^(n) ;
+      }
+      else {
 
+            int cur = power(x,n/2,mod) ;
 
-      while(true){
+            return 1LL*cur *cur % mod * x % mod ; // did mod before mod , cause of overflow ;
 
-           cur = 1LL * cur * x % MOD ;
-
-           if(vis[cur]){
-
-              start = find(v.begin() ,v.end() ,cur) - v.begin() ;
-
-              break ;
-           }
-
-           vis[cur] = true ;
-
-           v.push_back(cur) ;
       }
 
-
-      for(auto i : v){
-
-             cout << i << nl; 
-      }
-
-      cout << nl  ;
-
-      cout << "start : " << start << nl; 
-
-
-       if(n < start) return v[n] ;
-
-       int cycle_len = v.size() - start ;
-
-       n -= start ;
-
-      
-      return v[start + n % cycle_len] ;  
 }
 
 void solve ()
@@ -85,7 +62,7 @@ void solve ()
           
            for(int i = 0 ;i<n ;i++){
 
-               cout << power(a[i], b[i]) << nl; 
+               cout << power(a[i], b[i] , MOD) << nl; 
 
            }
 
