fd7902754402fa44a6182fc1719f96173c703fcf
farhad-mahmud <rixonahmed@gmail.com>
2025-07-04 16:43:48 +0600

    stick lengths

---
+++ CP 2025/cses_stick_lengths.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int  long long
+
+#define nl       "\n" 
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+// Constants
+const int N = 1e5 + 9;
+const int MOD = 1e9 + 7;
+
+void solve ()
+{
+
+          int n ; cin >> n ;
+
+          vector<int > p(n+1) ;
+
+          // most occuring freq ;
+
+          for(int i=1 ;i<=n;i++){
+
+                cin >> p[i] ;
+            
+          }
+    
+           sort(p.begin(),p.end()) ;
+
+           int median = p[(n+1)/2]  ;
+
+          int cnt = 0;
+
+          for(int i=1 ;i<=n;i++){
+
+                 cnt += abs(p[i] - median) ;
+          }
+
+
+          cout << cnt << nl; 
+
+
+}
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+        // cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
