8e86ccd39ffbe4496c52260a273a321224532394
farhad-mahmud <rixonahmed@gmail.com>
2025-07-15 02:59:17 +0600

    solved div3 d

--- CP 2025/2121D.cpp
+++ CP 2025/2121D.cpp
@@ -14,11 +14,67 @@ using namespace std;
 const int N = 1e5 + 9;
 const int MOD = 1e9 + 7;
 
-
 void solve ()
 {
-               
-       
+                int n ; cin >> n ;
+
+                vector<int> a(n+1) , b(n+1) ;
+
+                for(int i=1;i<=n;i++){
+                    cin >> a[i]  ;
+                }
+                for(int i=1;i<=n;i++){
+                     cin >> b[i] ;
+                }
+
+                vector<pair<int,int >> v  ;
+
+               int k = 0 ;
+
+               for(int i=1;i<=n;i++){
+
+                    if(a[i] > b[i]){
+
+                        swap(a[i],b[i]) ;
+                        k++ ;
+
+                        v.push_back({3,i}) ;
+                    }
+               }
+
+
+             for(int i=1;i<=n-1;i++){
+
+               for(int j=1;j<=n-1;j++){
+
+                    if(a[j] > a[j+1]){
+
+                        k++ ;
+                        swap(a[j] , a[j+1]);
+
+                        v.push_back({1,j}) ;
+                    }
+
+                    if(b[j] > b[j+1]){
+
+                         k++ ;
+                         swap(b[j],b[j+1]) ;
+
+                         v.push_back({2,j}) ;
+                    }
+
+                 }
+              }
+
+
+               cout << k << nl; 
+
+               for(auto i : v){
+
+                   cout << i.first << ' ' << i.second << nl;
+               }
+
+   
 }
 
 int32_t main() {
