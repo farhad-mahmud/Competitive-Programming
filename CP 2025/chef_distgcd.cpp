e8793c4f85034c0aef3ed07b03b5d43b36e9411f
farhad-mahmud <rixonahmed@gmail.com>
2025-08-05 15:32:33 +0600

    number theory practice

---
+++ CP 2025/chef_distgcd.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int  long long
+
+#define nl       "\n" 
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(a) for(auto &it: a) cerr<<it<<" "; cerr<<nl;
+// Constants
+const int N = 1e5 + 9;
+const int MOD = 1e9 + 7;
+
+
+void solve ()
+{
+             int a , b ; cin >> a >> b ;
+
+             int d= abs(a-b) ;
+
+             int cnt = 0 ;
+
+             for(int i=1;i*i <=d ;i++){
+
+                 if(d % i ==0){
+                     cnt++ ;
+
+                    if(i != d/i){
+                          cnt++ ;
+                    }
+
+                 }
+             }
+
+
+             cout << cnt << nl; 
+}
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+         cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
