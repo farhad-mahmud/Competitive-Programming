79a848558f1c18a82f547746f1681312591c6a67
farhad-mahmud <rixonahmed@gmail.com>
2025-08-06 09:24:07 +0600

    codechef

---
+++ CP 2025/chef_and.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int  long long
+
+#define nl       "\n" 
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+#define all(x)   x.begin(),x.end() 
+#define allr(x)  x.rbegin() ,x.rend()
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(a) for(auto &it: a) cerr<<it<<" "; cerr<<nl;
+// Constants
+const int N = 1e5 + 9;
+const int MOD = 1e9 + 7;
+
+
+void solve ()
+{
+          int n; cin >> n ;
+
+          int bits = 0 ;
+          int x = n ;
+
+          while(x!=0){
+             bits++ ;
+             x = x>>1 ;
+          }
+
+          int pow = 1 <<(bits -1) ;
+
+          if(n== pow){
+
+             cout << pow - (pow >> 1) << nl ;
+          }
+          else{
+              int  a= n - pow + 1 ;
+
+              int b= pow- (pow>> 1) ;
+
+              cout << max(a,b) << nl; 
+          }
+}
+
+int32_t main() {
+ios_base:: sync_with_stdio(0);
+cin.tie(0);
+
+         int t = 1 ;
+
+         cin >> t ;
+
+         while(t--){
+
+            solve() ;
+        
+        }
+
+
+    return 0;
+}
