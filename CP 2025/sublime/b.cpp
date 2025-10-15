7278cd2d13b602f9de739cec4dc499cdcd8029c1
farhad-mahmud <rixonahmed@gmail.com>
2025-10-15 22:28:58 +0600

    codechef contest

---
+++ CP 2025/sublime/b.cpp
@@ -0,0 +1,74 @@
+//---  Bismillahir Rahmanir Rahim ---//
+
+//------------------------------//
+//        Author: Farhad       //
+//------------------------------//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int  long long
+
+#define nl       "\n"
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+#define all(x)   x.begin(),x.end()
+#define allr(x)  x.rbegin() ,x.rend()
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(a) for(auto &it: a) cerr<<it<<" "; cerr<<nl;
+
+// constrains
+
+const int N = 1e5 + 9;
+const int MOD = 1e9 + 7;
+
+
+void solve ()
+{
+   int n ; cin >> n ;
+
+   int m = n - 1 ;
+
+   int pos = n ;
+
+   int i = 1 ;
+   // int k = n ;
+
+   while (m--) {
+
+      if (i % 2 == 0) { // if even ..
+         pos = pos + (n - i) ;
+
+
+      }
+      else {
+
+         pos = pos - (n - i) ;
+
+      }
+
+      i++ ;
+   }
+
+   cout <<  pos << nl ;
+}
+
+int32_t main() {
+   ios_base:: sync_with_stdio(0);
+   cin.tie(0);
+
+   int t = 1 ;
+
+   cin >> t ;
+
+   while (t--) {
+
+      solve() ;
+
+   }
+
+
+   return 0;
+}
