6b18a4d52e249634dae91809d885c5c4ec76a578
Farhad Mahmud <rixonahmed@gmail.com>
2026-02-24 05:19:59 +0600

    implementation

--- CP 2025/round1082c.cpp
+++ CP 2025/round1082c.cpp
@@ -24,8 +24,88 @@ const int MOD = 1e9 + 7;
 
 void solve ()
 {  
+         int  n ; cin >> n;
+         vector<int > a(n) ;for(int i=0;i<n;i++)cin >> a[i] ;
 
- 
+         map<int,int > hash ;
+         map<int,bool> par ;
+
+         for(auto x : a){
+               //hash[x]++ ;
+               par[x] = false ;
+         }
+
+         int cnt = 0 ;
+         for(int i=0;i<n;i++){
+              
+
+              if(i==0){
+                  par[a[i]] = true ;
+                  hash[a[i]]++ ;
+                  cnt++ ;
+                  continue ;
+              }
+
+              // if(i==2){
+              //     cout << "h" << nl; 
+              //     cout << a[i]-1 << nl;
+              //     cout << hash[a[i]-1] << nl ;
+              // }
+
+              if(hash[a[i]-1]>=1 && par[a[i]-1]){ // age ai-1 jdi exist kore ar parent hoy..
+                  //child o ache parent o ache.
+                 // ekhon.. first child hole.parent ager index ei thakte hbe..
+                  if(hash[a[i]] == 0){  // mane ai, first child..
+                        if(a[i]-1 == a[i-1]){ // parent jodi ager index e hoy..
+                              // count hbena
+                           hash[a[i]]++ ;
+                           continue ;
+                        }
+                        else {
+                             cerr << i << nl ;
+                             cnt++ ;
+                        }
+                  }
+                  else {  // first child na hole..
+                        hash[a[i]]++;
+                        continue ;
+                  }
+                  //cerr << "he " << i << nl ;
+                  hash[a[i]]++ ;
+                  continue;
+              }
+              else if(hash[a[i]-1]>=1 && !par[a[i]-1]){  // ai-1 exist kore but parrent na..
+                  // cerr << "four" << i << nl ;
+                   if(hash[a[i]] == 0){  // mane ai, first child..
+                        if(a[i]-1 == a[i-1]){ // parent jodi ager index e hoy..
+                              // count hbena
+                           hash[a[i]]++ ;
+                           continue ;
+                        }
+                        else {
+                         
+                             cnt++ ;
+                        }
+                  }
+                  else {  // first child na hole..
+                        hash[a[i]]++;
+                        continue ;
+                  }
+                  hash[a[i]]++ ;
+                  continue ;
+              }
+              else if(hash[a[i]-1]== 0){   // parent.. exist i korena..
+                 
+                    par[a[i]] = true ; // new parent..
+                    cnt++ ;
+
+                    //cerr << hash[a[i]-1] << nl;
+                    hash[a[i]]++ ;
+              }
+
+         }
+
+         cout << cnt << nl; 
 }
 
 int32_t main() {
@@ -34,7 +114,7 @@ int32_t main() {
 
    int t = 1 ;
 
-   // cin >> t ;
+   cin >> t ;
 
    while (t--) {
 
