b7ee6f31ec313eddefbb5d823136279ed7b136a4
Farhad Mahmud <rixonahmed@gmail.com>
2026-03-03 04:05:57 +0600

    number thoery

--- CP 2025/round1084e.cpp
+++ CP 2025/round1084e.cpp
@@ -17,18 +17,99 @@ using namespace std;
 
 // constrains
 
-const int N = 1e5 + 9;
+const int N = 1e7 + 9;
 const int MOD = 1e9 + 7;
 
+bool vis[N] ;
+vector<int> prime ;
 
+void sieve() {
+
+    for (int i = 2; i * i < N; i++) {
+        if (vis[i]) continue;
+
+        for (int j = i * i; j < N; j += i) {
+            vis[j] = true;
+        }
+    }
+}
+bool pf_sqr(int n){
+     if(n<0) return false ;
+     int r = sqrtl(n);
+     return r*r == n || (r+1)*(r+1) == n; 
+}
+bool istwo(int n){
+     return n > 0 && (n & (n-1))== 0;
+}
+bool semiprime(int n) {
+    int count = 0;      
+    int temp = n;
+
+    for (int i = 2; i * i <= temp; i++) {
+        while (temp % i == 0) {
+            temp /= i;
+            count++;
+            if (count > 2) return false; 
+        }
+    }
+    if (temp > 1) count++;  
+
+    return count == 2;   
+}
 void solve ()
 {  
             int n;  cin >> n ;
             vector<int > a(n); for(int i=0;i<n;i++)cin>> a[i]; 
 
-            
+            vector<int > p;
+
+            vector<int > q = a ;
+            sort(all(q));
+            if(a==q){
+                 cout << "Bob" << nl;
+                 return ;
+            }
+            for(int i=0;i<n;i++){
+                 if(!vis[a[i]]){
+                      p.push_back(a[i]) ;
+                 }
+            }
+
+            vector<int > c= p;
+            sort(all(c));
+            //output(c) ;
+            if(c!= p){
+                 cout << "Alice" << nl;
+                 return ;
+            }   
+
+            vector<int > nw ;
+            for(int i=0;i<n;i++){
+                if(!istwo(a[i]) && !pf_sqr(a[i])){
+                      if(vis[a[i]]){
+                       nw.push_back(a[i]) ;
+                     }
+                }
+            }
+
+            //output(nw);  
+            bool f = true ;
+
+            int sz = nw.size();
+            for(int i=0;i<sz;i++){
+                  if(semiprime(nw[i])){
+                        f = false ;
+                        break ;
+                  }
+            }
 
 
+            if(f){
+                cout << "Bob" << nl; 
+            }
+            else {
+                cout << "Alice" << nl ;
+            }
 }
 
 int32_t main() {
@@ -37,7 +118,8 @@ int32_t main() {
 
    int t = 1 ;
 
-   // cin >> t ;
+   sieve();
+   cin >> t ;
 
    while (t--) {
 
--- CP 2025/test1.cpp
+++ CP 2025/test1.cpp
@@ -25,48 +25,6 @@ void solve ()
 {  
 
  
-      // for(int i=0;i<s.size();i++){
-      //      if(s[i] == s[i+1]){
-      //          s[i] = '*';
-      //          s[i+1] = '*' ;
-      //          i++ ;
-      //      }
-      // }
-
-
-      // //cerr <<s  << nl ;
-      // string t ;
-      // for(int i=0;i<n;i++){
-      //      if(s[i] != '*'){
-      //          t+= s[i] ;
-      //      }
-      // }
-
-    // // cerr << t << nl; 
-    //   string t = s ;
-
-    //   if(t.size() % 2 == 1){ 
-    //       no ;
-    //       return ;
-    //   }
-    //   int l = 0 , r =t.size()-1 ; 
-    //   bool f = true ;
-    //   while(l < r){
-    //        if(l+1 <= r && t[l] == t[l+1]){
-    //            l+=2 ;
-    //        }
-    //        else if(t[r]== t[r-1]){
-    //             r-=2 ;
-    //        }   
-    //        else if(t[l] == t[r]){
-    //             l++ ;
-    //             r-- ;
-    //        }
-    //        else {
-    //            f = false ;
-    //            break ;
-    //        }
-    //   }
 }
 
 int32_t main() {
