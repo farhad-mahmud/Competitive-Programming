108a315022dd1ea4658a06bdd931c9ceb8df0a05
Farhad Mahmud <rixonahmed@gmail.com>
2026-03-18 07:46:04 +0600

    cp

--- CP 2025/cf_addmodulo.cpp
+++ CP 2025/cf_addmodulo.cpp
@@ -26,7 +26,44 @@ void solve ()
       int n ; cin >> n ;
       vector<int> a(n); for(int i=0;i<n;i++)cin >> a[i];
 
-      
+      vector<pair<int,int > > v ;
+      //ends with 0 , same catagory..
+      for(int i=0;i<n;i++){
+            if(a[i] < 10){
+                if(a[i] == 1 || a[i] == 3){
+                      a[i]+= a[i];
+
+                      v.push_back({0,a[i]});
+
+                }
+                else if(a[i] == 7){
+                     v.push_back({1,4});
+                }
+                else{
+                     v.push_back({0,a[i]});
+                }
+            }
+            else{
+
+                 int last = a[i] % 10 ;   
+                 int pos = a[i] / 10 ;
+
+                 if(last == 1 || last == 3){
+                      last+=last ;
+                      v.push_back({pos,last});
+                 }
+                 else if(last == 7){
+                      v.push_back({pos+1,4});
+                 }
+                 else{
+                     v.push_back({pos,last});
+                 }
+
+            }
+      }
+
+
+
 }
 
 int32_t main() {
