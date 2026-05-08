09c53d33490ae8da776c85e8b7d3dfbe0e41d7d3
Farhad Mahmud <rixonahmed@gmail.com>
2026-05-09 04:53:06 +0600

    dp

--- CP 2025/cf_basketball_exercise.cpp
+++ CP 2025/cf_basketball_exercise.cpp
@@ -16,20 +16,61 @@ using namespace std;
 #define output(a) for(auto &it: a) cerr<<it<<" "; cerr<<nl;
 
 // constrains
-
-const int N = 1e9 + 9;
 const int MOD = 1e9 + 7;
 
+const int N = 1e5+5 ;
+
+
+int a[N],b[N] ;
+int dp[N][3] ;
+int n ;
+// j = prev day..
+
+int f(int i, int r){
+
+      if(i == n+1) return 0 ;
+      
+      if(dp[i][r] != -1) return dp[i][r] ;
+
+
+      int ta = 0 , tb = 0 ;
+
+      if(r != 0){ // prev selected row was not 0 .
+
+            // ei element nibo naki nibona..
+         // current element na nile.. ei porer state e prev value.. hobe. element 1
+         // 
+            ta = max(f(i+1,1), a[i] + f(i+1 , 0)) ;
+      }
+      if(r != 1){   // prev selected row was not 1 ..
+
+            tb = max(f(i+1,0),  b[i] + f(i+1, 1)) ;
+      }
+
+   
+      int res = max(ta,tb) ;
+
+      dp[i][r] = res ;
+
+
+      return dp[i][r] ;
+ }
 
 void solve ()
 {  
-            
-            int n ; cin >> n ;
-            vector<int > h1 ; for(int i=0;i<n;i++)cin >> h1[i];
-              vector<int > h2 ; for(int i=0;i<n;i++)cin >> h2[i];
-
+               cin >>n ;
               
+               for(int i=1;i<=n;i++){
+                    cin >> a[i]  ;
+               }
+               for(int i=1;i<=n;i++){
+                    cin >> b[i]  ;
+               }
 
+               memset(dp, -1 , sizeof dp) ;
+
+
+               cout << f(1,2) << nl ;
 }
 
 int32_t main() {
@@ -38,7 +79,7 @@ int32_t main() {
 
    int t = 1 ;
 
-   cin >> t ;
+   //cin >> t ;
 
    while (t--) {
 
