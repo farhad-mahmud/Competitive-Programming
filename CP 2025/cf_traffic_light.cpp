21987ab751600a7683211473e6479bc66246b824
Farhad Mahmud <rixonahmed@gmail.com>
2026-05-15 22:53:02 +0600

    string

--- CP 2025/cf_traffic_light.cpp
+++ CP 2025/cf_traffic_light.cpp
@@ -25,11 +25,78 @@ void solve ()
 {  
           int n ; cin >> n ;
 
-          string c ; cin >> c ;
+          char c ; cin >> c ;
 
           string s ; cin >> s ;
 
-          
+          int first_g = 0 ;
+
+
+          vector<int > v ;
+
+          if(c =='g' || n == 1){
+               cout << 0 << nl ;
+               return ;
+          }
+
+          for(int i=0;i<n;i++){
+               if(s[i] == 'g' && first_g == 0){
+                     first_g = i+1 ;
+                    
+               }
+
+               if(s[i] == 'g'){
+                    v.push_back(i+1) ;
+               }
+          }
+
+          // output(v) ;
+          // cerr << first_g << nl;
+
+
+          vector<int > dis ;
+          int sz = v.size() ;
+
+
+          for(int i=0;i<n;i++){
+
+               if(s[i] == c){
+
+                  // cerr << 1 << nl ;
+                   bool f = false ;
+                   for(int j=0;j<sz; j++){
+
+
+                        if(v[j] > i+1 ){
+                            int d = v[j] - (i+1 );
+                           
+                            f = true ;
+
+                            dis.push_back(d) ;
+
+                            break ;
+
+                        }
+
+                   }
+
+                    if(!f){  // r er right e kono g nei.
+
+                            int d = (n-(i+1)) + first_g ;
+
+                            dis.push_back(d) ;
+                     }
+               }
+          }
+
+
+          //output(dis) ;
+
+          int mx = *max_element(all(dis)) ;
+
+
+          cout << mx << nl ;
+
 
 }
 
@@ -39,7 +106,7 @@ int32_t main() {
 
    int t = 1 ;
 
-   //cin >> t ;
+   cin >> t ;
 
    while (t--) {
 
