44e93da9950578621ce34a0040e19e6b458fdb8d
Farhad Mahmud <rixonahmed@gmail.com>
2026-06-11 14:30:08 +0600

    dp

--- CP 2025/neetcode_palindrom.cpp
+++ CP 2025/neetcode_palindrom.cpp
@@ -1,86 +1,37 @@
-//---  Bismillahir Rahmanir Rahim ---//
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n == 0) return "";
 
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int start = 0, max_len = 1;
 
-#include <bits/stdc++.h>
-using namespace std;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
 
-#define int  long long
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                start = i;
+                max_len = 2;
+            }
+        }
 
-#define nl       "\n"
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (len > max_len) {
+                        start = i;
+                        max_len = len;
+                    }
+                }
+            }
+        }
 
-#define yes cout << "YES\n";
-#define no cout << "NO\n";
-#define all(x)   x.begin(),x.end()
-#define allr(x)  x.rbegin() ,x.rend()
-#define dbug(x) cerr << (#x) << " is " << (x) << nl;
-#define output(a) for(auto &it: a) cerr<<it<<" "; cerr<<nl;
-
-// constrains
-
-const int N = 1e6 + 5 ;
-
-const int MOD = 1e9 + 7;
-
-void solve ()
-{  
-         string s ; cin >> s ;
-
-         int l = s.length();
-
-         string t = "" ;
-
-         //bool f = false ;
-
-         vector < string > p ;
-
-         for(int i=0;i<l;i++){
-              
-              t = s[i] ;
-             
-             for(int j = i+1;j<l;j++){
-                  t += s[j] ;
-                  string rev = t ;
-                  reverse(all(rev)) ;
-
-                  if(t == rev){ // palindrom..
-                        p.push_back(t) ;
-                  }
-
-              }
-         }
-
-
-         sort(all(p)) ;
-
-         string ans  ;
-         int mx = 0 ;
-         for(auto ss : p){
-               
-               if(ss.size() > mx){
-                    mx = ss.size();
-                    ans = ss ;
-               }
-         }  
-
-         cout << ans << nl; 
-
-
-}
-
-int32_t main() {
-   ios_base:: sync_with_stdio(0);
-   cin.tie(0);
-
-   int t = 1 ;
-
-   //cin >> t ;
-
-   while (t--) {
-
-      solve() ;
-
-   }
-
-
-   return 0;
-}
+        return s.substr(start, max_len);
+    }
+};
