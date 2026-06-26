12eaecb3fd39909fa75b62cd0218120f6ba4a8a8
Farhad Mahmud <rixonahmed@gmail.com>
2026-06-26 16:13:50 +0600

    dp

--- CP 2025/trans_a_b.cpp
+++ CP 2025/trans_a_b.cpp
@@ -1,13 +1,8 @@
-//---  Bismillahir Rahmanir Rahim ---//
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
 #define int  long long
-
 #define nl       "\n"
-
 #define yes cout << "YES\n";
 #define no cout << "NO\n";
 #define all(x)   x.begin(),x.end()
@@ -15,60 +10,41 @@ using namespace std;
 #define dbug(x) cerr << (#x) << " is " << (x) << nl;
 #define output(a) for(auto &it: a) cerr<<it<<" "; cerr<<nl;
 
-// constrains
-
-const int N = 1e9+ 5 ;
-
 const int MOD = 1e9 + 7;
-int dp[N] ;
+map<int, int> dp;
 
 int a, b; 
 int f(int cur){
-
      if(cur > b){
         return 0 ;
-     }
+     }   
+
+     cerr << cur << nl ;
      if(cur == b){
-
+        return 1;
      }
-
-     if(dp[i] != -1){
-         return dp[i] ;
+     if(dp.count(cur)){
+         return dp[cur] ;
      }
-
      int ans = f(cur*2) ;
-
      ans = ans + f(cur*10 + 1) ;
 
-     return dp[i] = ans ;
-
-
+     return dp[cur] = ans ;
 }
+
 void solve ()
 {  
         cin >> a >> b ;
-
-        memset(dp , -1 , sizeof dp) ;
-
-
+        dp.clear();
         cout << f(a) << nl;
-
 }
 
 int32_t main() {
    ios_base:: sync_with_stdio(0);
    cin.tie(0);
-
    int t = 1 ;
-
-   //cin >> t ;
-
    while (t--) {
-
       solve() ;
-
    }
-
-
    return 0;
 }
